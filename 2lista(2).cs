1cc16a03d3337f628009e06da49a4cda78663c07
viitto <78694741+viitto@users.noreply.github.com>
2021-12-06 23:56:18 -0300

    Create 2lista(2).cs

---
+++ 2lista(2).cs
@@ -0,0 +1,11 @@
+using System;
+
+class MainClass
+{
+  public static void Main(string[] args)
+  {
+    Console.WriteLine("digite seu nome completo");
+    string b = Console.ReadLine();
+    Console.WriteLine("Bem-vindo ao C#, " + b);
+  }
+}
