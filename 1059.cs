7de8d730c8a83e0999fb164c8ba074bf8e3acf9d
viitto <78694741+viitto@users.noreply.github.com>
2022-01-14 20:13:09 -0300

    Add files via upload

---
+++ 1059.cs
@@ -0,0 +1,13 @@
+using System; 
+
+public class Program {
+
+  public static void Main(string[] args) {
+    for(int a = 2; a <= 100 ; a++)
+    {
+      if(a % 2 == 0){
+      Console.WriteLine(a);
+      }
+    }
+  }
+}
---
+++ 1080.cs
@@ -0,0 +1,23 @@
+using System;
+
+public class Example
+{
+   public static void Main()
+   {
+    int x, M, P;
+
+    int[] a = new int[10];
+    foreach(int i in a)
+    {
+      Console.ReadLine();
+    }
+    
+    Console.WriteLine(a);
+      
+    
+
+
+
+    // Console.Read();
+   }
+}
